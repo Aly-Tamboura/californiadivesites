2c7859955c5d121fd4238dd9ad4df0a823cc47fe
Aly-Tamboura <alytamboura7370@gmail.com>
2017-06-22 19:30:24 -0700

    file setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = process.ENV || 3000;
+
+app.use(express.static('client'))
+
+app.listen(port, function (argument) {
+	console.log('diving app running on port', port);
+})
